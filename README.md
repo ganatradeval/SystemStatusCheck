8c38c4ba2947b94f376b735d82973d1bdd26729b
Deval <ganatradeval@gmail.com>
2018-07-27 18:09:00 +0530

    Added script and initial Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Health Chech by Ping 
+
+	This simple script will ping you given hosts and returns message onto slack if server is down(Ping is not working).
+
+## Input Variables 
+host = [<IP_Address here>] string Seperated by comma
+SLACK_CHANNEL="<Channel Name>"
+HOOK_URL= "<URL of channel>"
+
---
+++ statuscheck.py
@@ -0,0 +1,39 @@
+from platform import system as system_name
+from subprocess import getstatusoutput
+import json,urllib3
+
+host = ['192.168.0.1','192.168.0.2'] #Change ip/hostname of your server/application
+
+SLACK_CHANNEL="#<Slack Channel Name>"
+HOOK_URL="<URL of Channel>"
+
+#To limit ping request
+param = '-n' if system_name().lower()=='windows' else '-c'
+
+command = [ ['ping', param, '1', i]	for i in host]
+
+
+message=''
+for i in command:
+	status,out = getstatusoutput(i)
+	if status == 1 :
+		print (i[3], "is NOT working")
+	else:
+		if out.find('time=') == -1:
+			print (i[3], "is NOT working\n")
+			message += i[3] + "is NOT working"
+		else:
+			print (i[3], "is working fine")
+			message += i[3] + "is NOT working\n"
+#Create format of slack message request
+slack_message = {
+       'channel': SLACK_CHANNEL,
+       'text': message,
+       'username': 'System Check',      #you cam modify this settings
+       'icon_emoji': ":ghost:"          #also this can be modified
+   }
+
+#Send the POST request using urllib
+http=urllib3.PoolManager()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)     #optional but use if Shows warning of Certificate 
+response = http.request('POST', HOOK_URL, headers={'Content-Type':'application/json'}, body=json.dumps(slack_message) )
